b45168d3fb6c80e3fedecc21f3d1ed8ccb4cff4c
Tim <tbarry2@andrew.cmu.edu>
2022-06-19 11:42:45 -0400

    add a synthetic gRNA expression matrix

--- data-raw/create_ondisc_matrices.R
+++ data-raw/create_ondisc_matrices.R
@@ -20,3 +20,28 @@ to_save_metadata <- paste0(ondiscdata_dir, "inst/extdata/odm/gRNA/metadata.rds")
 odm <- create_ondisc_matrix_from_R_matrix(r_matrix = gene_mat, barcodes = gene_barcodes,
                                           features_df = gene_feature_df, odm_fp = to_save_odm,
                                           metadata_fp = to_save_metadata)
+
+# synthesize gRNA count data
+set.seed(4)
+n_cells <- 10000
+n_gRNAs <- 100
+gRNA_exp <- replicate(n = n_cells, expr = {
+  v <- integer(n_gRNAs)
+  rand_idxs <- sample(x = seq(1, n_gRNAs), size = sample(x = seq(1,4), size = 1), replace = FALSE)
+  v[rand_idxs] <- rpois(n = length(rand_idxs), lambda = 100)
+  v
+}, simplify = TRUE)
+gRNA_ids <- paste0("gRNA_", seq(1, n_gRNAs))
+cell_ids <- paste0("cell_", seq(1, n_cells))
+gRNA_groups <- rep(paste0("gRNA_group_", seq(1, n_gRNAs/2)), each = 2)
+to_save_odm <- paste0(ondiscdata_dir, "inst/extdata/odm/gRNA_expression/matrix.odm")
+to_save_metadata <- paste0(ondiscdata_dir, "inst/extdata/odm/gRNA_expression/metadata.rds")
+
+odm <- create_ondisc_matrix_from_R_matrix(r_matrix = gRNA_exp,
+                                          barcodes = cell_ids,
+                                          features_df = data.frame(gRNA_ids),
+                                          odm_fp = to_save_odm,
+                                          metadata_fp = to_save_metadata)
+odm <- odm |> ondisc::mutate_feature_covariates(gRNA_group = gRNA_groups)
+save_odm(odm = odm, metadata_fp = to_save_metadata)
+
